f381b8efea5b24d2015eb0361ef08125241ef7f3
Luis Olivares <luissolivaresp@gmail.com>
2023-05-14 17:31:44 -0500

    🟢 Add admin authentication

--- super_hero_API/api/viewsets.py
+++ super_hero_API/api/viewsets.py
@@ -8,14 +8,14 @@ from retrying import retry
 class SuperheroViewSet(viewsets.ModelViewSet):  
     serializer_class = SuperheroSerializer
     queryset = Superhero.objects.all().order_by('id')
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class SupervillainViewSet(viewsets.ModelViewSet):
     serializer_class = SupervillainSerializer
     queryset = Supervillain.objects.all().order_by('id')
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class LocationViewSet(viewsets.ModelViewSet):
     serializer_class = LocationSerializer
     queryset = Location.objects.all().order_by('id')
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
